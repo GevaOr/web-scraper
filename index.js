d245c21bdfbd04b4c13ee73d5ccd00ef15e63f86
Geva <70754020+GevaOr@users.noreply.github.com>
2021-09-30 17:42:31 +0300

    finalized template

--- index.js
+++ index.js
@@ -10,14 +10,19 @@ const app = express();
 const filename = ''; // filename to write to (without extension)
 const url = ""; // URL to scrape
 const htmlClassQuery = ""; // Class of the HTML element to scrape
-const headers = [];  // Array of header titles in the xls file. Example: ['Title', 'Description', 'Link'...]
+const headers = [];  // Array of header titles in the csv file. Example: ["name", "location", "phone"...]
+
+const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
 
 const createHeaderRowString = (headers) => {
-    const headerRow = '';
+    let headerRow = '';
     headers.forEach((header) => {
-        headerRow += `${capitalizeFirstLetter(header)}`;
+        headerRow += `"${capitalizeFirstLetter(header)}",`;
     });
-    headerRow += '\n';
+    headerRow = headerRow.substring(0, headerRow.length - 1) + "\n";  // remove trailing comma and add newline
     return headerRow;
 }
 
@@ -27,27 +32,25 @@ const addHeaderRow = (headers, fileStream) => {
     }
     const headerRow = createHeaderRowString(headers);
     fileStream.write(headerRow);
-    // fileStream.end();
-};
-
-capitalizeFirstLetter = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
 axios(url)
     .then(resp => {
-        const scrapedHtml = resp.data;
-        const $ = cheerio.load(scrapedHtml);
-        let stream = fs.createWriteStream(`${filename}.xls`);
+        const html = resp.data;
+        const $ = cheerio.load(html);
+        let stream = fs.createWriteStream(`${filename}.csv`);
 
         addHeaderRow(headers, stream);
 
-        // Add logic to scrape data from the HTML
-
+        // Add logic to scrape data from the HTML and write to the file stream.
+        // For example:
         // $(`.${htmlClassQuery}`)
-            // .each(() => {
-                // stream.write($(this).text().trim() + "\t");
-            // })
+        //     .each((_i, elem) => {
+        //         const name = $(elem).find('.name').text();
+        //         const location = $(elem).find('.location').text();
+        //         const phone = $(elem).find('.phone').text();
+        //         stream.write(`"${name}","${location}","${phone}"\n`);
+        //     })
 
         stream.end();
     })
